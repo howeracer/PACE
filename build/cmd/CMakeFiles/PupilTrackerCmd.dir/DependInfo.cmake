a07cb08ef18d8ff0f0f7f7a4d786abace431c1f7
William Ryder <billyryder5@gmail.com>
2018-03-26 14:24:46 -0400

    "up to date eye tracking code"

--- build/cmd/CMakeFiles/PupilTrackerCmd.dir/DependInfo.cmake
+++ build/cmd/CMakeFiles/PupilTrackerCmd.dir/DependInfo.cmake
@@ -1,26 +1,25 @@
 # The set of languages for which implicit dependencies are needed:
-SET(CMAKE_DEPENDS_LANGUAGES
+set(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-SET(CMAKE_DEPENDS_CHECK_CXX
-  "/home/j/pupiltracker/cmd/PupilTrackerCmd.cpp" "/home/j/pupiltracker/build/cmd/CMakeFiles/PupilTrackerCmd.dir/PupilTrackerCmd.cpp.o"
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/billy/Documents/PACE/cmd/PupilTrackerCmd.cpp" "/home/billy/Documents/PACE/build/cmd/CMakeFiles/PupilTrackerCmd.dir/PupilTrackerCmd.cpp.o"
   )
-SET(CMAKE_CXX_COMPILER_ID "GNU")
-
-# Targets to which this target links.
-SET(CMAKE_TARGET_LINKED_INFO_FILES
-  "/home/j/pupiltracker/build/lib/CMakeFiles/pupiltracker.dir/DependInfo.cmake"
-  "/home/j/pupiltracker/build/cmd/dlib_build/CMakeFiles/dlib.dir/DependInfo.cmake"
-  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
-SET(CMAKE_C_TARGET_INCLUDE_PATH
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "../3rdParty/dlib/.."
-  "/usr/local/include/opencv"
-  "/usr/local/include"
+  "/usr/include/opencv"
   "../lib"
   )
-SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/billy/Documents/PACE/build/lib/CMakeFiles/pupiltracker.dir/DependInfo.cmake"
+  "/home/billy/Documents/PACE/build/cmd/dlib_build/CMakeFiles/dlib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
